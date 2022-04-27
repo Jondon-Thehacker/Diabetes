bc0f208fe5b50e1f1efbc48785642e8d7eea5f19
Jondon-Thehacker <jonathan.max.michelsen@hotmail.com>
2022-04-27 16:03:28 +0200

    Graph

--- Vue/src/components/GraphView.vue
+++ Vue/src/components/GraphView.vue
@@ -1,13 +1,14 @@
 <template>
   <p>Graphs</p>
   <div>
-    <Line :chart-data="chartData" />
+    <Line :chart-data="chartData" :plugins="plugins" />
   </div>
 </template>
 
 <script>
-import { Line } from 'vue-chartjs'
-import { Chart as   Chart,
+import { Line } from "vue-chartjs";
+import {
+  Chart as Chart,
   ArcElement,
   LineElement,
   BarElement,
@@ -31,10 +32,13 @@ import { Chart as   Chart,
   Legend,
   Title,
   Tooltip,
-  SubTitle } from 'chart.js'
+  SubTitle,
+} from "chart.js";
 
+import annotationPlugin from "chartjs-plugin-annotation";
 
-Chart.register(ArcElement,
+Chart.register(
+  ArcElement,
   LineElement,
   BarElement,
   PointElement,
@@ -57,33 +61,59 @@ Chart.register(ArcElement,
   Legend,
   Title,
   Tooltip,
-  SubTitle)
+  SubTitle,
+  annotationPlugin
+);
 
 export default {
-  name: 'BarChart',
+  name: "BarChart",
   components: { Line },
   data() {
     return {
       chartData: {
-        labels: [ '1990-03-03T23:00:00', '1990-03-04T23:00:00', '1990-03-05T23:00:00','1990-03-06T23:00:00','1990-03-07T23:00:00','1990-03-08T23:00:00','1990-03-09T23:00:00','1990-03-10T23:00:00'],
+        labels: [
+          "1990-03-03T23:00:00",
+          "1990-03-04T23:00:00",
+          "1990-03-05T23:00:00",
+          "1990-03-06T23:00:00",
+          "1990-03-07T23:00:00",
+          "1990-03-08T23:00:00",
+          "1990-03-09T23:00:00",
+          "1990-03-10T23:00:00",
+        ],
         datasets: [
           {
-            label: 'CGM Levels',
-            backgroundColor: '#f87979',
-            data: [70, 30, 49,135,80,90,100,200]
-          }
-        ]
-      }
-    }
-  }
-}
+            label: "CGM Levels",
+            backgroundColor: "#f87979",
+            data: [120, 30, 49, 135, 80, 90, 100, 200],
+          },
+        ],
+      },
+      plugins: {
+        autocolors: false,
+        annotation: {
+          annotations: {
+            line1: {
+              type: "line",
+              yMin: 90,
+              yMax: 90,
+              borderColor: "rgb(252, 99, 132)",
+              borderWidth: 3,
+            },
+          },
+        },
+      },
+    };
+  },
+};
 </script>
 
 <style>
-  .container {
-      width: 900px ;
-      margin-left: auto ;
-      margin-right: auto ;
-      padding-top: 10%;
-      }
+.container {
+  width: 500;
+  height: 500;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 10%;
+}
 </style>
