845f5816c4638aaf1016943c8c101b441b8320d1
Lomo0841 <lomo0841@gmail.com>
2022-06-02 15:42:06 +0200

    Minor fixes to tests

--- src/main/java/diabetes/MeasurementControllerTest.java
+++ src/main/java/diabetes/MeasurementControllerTest.java
@@ -396,7 +396,7 @@ public class MeasurementControllerTest {
 
     @Test
     public void getCountAbove() throws Exception{
-        List<Measurement> jonathanMeasurement = new ArrayList<>(Arrays.asList(CGM0, CGM1, CGM2, CGM3, CGM4));
+        List<Measurement> jonathanMeasurement = new ArrayList<>(Arrays.asList(CGM1, CGM2, CGM3, CGM4));
         List<Patient> simonPatients = new ArrayList(Arrays.asList(Jonathan, EmilL));
         Jonathan.setMeasurements(jonathanMeasurement);
 
@@ -563,7 +563,7 @@ public class MeasurementControllerTest {
         Mockito.when(patientRepository.findById(Mockito.anyLong()))
                 .thenReturn(MockResponse2);
 
-        RequestBuilder request = MockMvcRequestBuilders.get("/api/v1/Doctors/{doctorId}/{patientId}/{dataType}/{startDate}/{endDate}/{aggregateFunction}", 1L,2L, "CGM","2022-04-13 00:00","2022-04-19 00:00","barChart", 5L);
+        RequestBuilder request = MockMvcRequestBuilders.get("/api/v1/Doctors/{doctorId}/{patientId}/{dataType}/{startDate}/{endDate}/summary/{type}/{stepSize}", 1L,2L, "CGM","2022-04-13 00:00","2022-04-19 00:00","barChart", 5L);
 
         MvcResult result = mockMvc.perform(request).andReturn();
 
