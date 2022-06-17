509653f7993355c32733128c3e79f3a0db349fd1
Lomo0841 <lomo0841@gmail.com>
2022-06-17 16:43:42 +0200

    Fixed error when changing graph for time interval with no measurements

--- Vue/src/components/visualization/KeyValues.vue
+++ Vue/src/components/visualization/KeyValues.vue
@@ -52,57 +52,58 @@ export default {
           method: 'get',
           url: 'http://localhost:8080/api/v1/Doctors/' + this.doctorId + '/' + this.patientId + '/' + this.measurementType + '/' + this.timeInterval + '/summary/keyValues/5'
         }).then(res => {
-          console.log(res.data.keyValues)
-
-          switch(this.measurementType){
-              case 'EXERCISE':
-                this.gcv = null
-                this.gmi = null
-                this.avg = res.data.keyValues.Average.toPrecision(4) + ' %';
-                this.stddiv = res.data.keyValues.Sd.toPrecision(4) + ' %';
-                this.min = res.data.keyValues.Min.toPrecision(4) + ' %';
-                this.max = res.data.keyValues.Max.toPrecision(4) + ' %';
-                break;
-              case 'CGM':
-                this.gcv = res.data.keyValues.GV.toPrecision(4) + ' %'
-                this.gmi = res.data.keyValues.GMI.toPrecision(4) + ' mmol/mol'
-                this.avg = res.data.keyValues.Average.toPrecision(4) + ' mmol/L';
-                this.stddiv = res.data.keyValues.Sd.toPrecision(4) + ' mmol/L';
-                this.min = res.data.keyValues.Min.toPrecision(4) + ' mmol/L';
-                this.max = res.data.keyValues.Max.toPrecision(4) + ' mmol/L';
-                break;
-              case 'MEALS':
-                this.gcv = null
-                this.gmi = null
-                this.avg = res.data.keyValues.Average.toPrecision(4) + ' g CHO';
-                this.stddiv = res.data.keyValues.Sd.toPrecision(4) + ' g CHO';
-                this.min = res.data.keyValues.Min.toPrecision(4) + ' g CHO';
-                this.max = res.data.keyValues.Max.toPrecision(4) + ' g CHO';
-                break;
-              case 'BASAL':
-                this.gcv = null
-                this.gmi = null
-                this.avg = res.data.keyValues.Average.toPrecision(4) + ' mU/min';
-                this.stddiv = res.data.keyValues.Sd.toPrecision(4) + ' mU/min';
-                this.min = res.data.keyValues.Min.toPrecision(4) + ' mU/min';
-                this.max = res.data.keyValues.Max.toPrecision(4) + ' mU/min';
-                break;
-              case 'BOLUS':
-                this.gcv = null
-                this.gmi = null
-                this.avg = res.data.keyValues.Average.toPrecision(4) + ' U';
-                this.stddiv = res.data.keyValues.Sd.toPrecision(4) + ' U';
-                this.min = res.data.keyValues.Min.toPrecision(4) + ' U';
-                this.max = res.data.keyValues.Max.toPrecision(4) + ' U';
-                break;
-              default:
-                this.gcv = null
-                this.gmi = null                
-                this.avg = res.data.keyValues.Average.toPrecision(4)
-                this.stddiv = res.data.keyValues.Sd.toPrecision(4)
-                this.min = res.data.keyValues.Min.toPrecision(4)
-                this.max = res.data.keyValues.Max.toPrecision(4)
-            }
+          console.log(res.data)
+          if(res.data != "" && res.data != undefined){
+            switch(this.measurementType){
+                case 'EXERCISE':
+                  this.gcv = null
+                  this.gmi = null
+                  this.avg = res.data.keyValues.Average.toPrecision(4) + ' %';
+                  this.stddiv = res.data.keyValues.Sd.toPrecision(4) + ' %';
+                  this.min = res.data.keyValues.Min.toPrecision(4) + ' %';
+                  this.max = res.data.keyValues.Max.toPrecision(4) + ' %';
+                  break;
+                case 'CGM':
+                  this.gcv = res.data.keyValues.GV.toPrecision(4) + ' %'
+                  this.gmi = res.data.keyValues.GMI.toPrecision(4) + ' mmol/mol'
+                  this.avg = res.data.keyValues.Average.toPrecision(4) + ' mmol/L';
+                  this.stddiv = res.data.keyValues.Sd.toPrecision(4) + ' mmol/L';
+                  this.min = res.data.keyValues.Min.toPrecision(4) + ' mmol/L';
+                  this.max = res.data.keyValues.Max.toPrecision(4) + ' mmol/L';
+                  break;
+                case 'MEALS':
+                  this.gcv = null
+                  this.gmi = null
+                  this.avg = res.data.keyValues.Average.toPrecision(4) + ' g CHO';
+                  this.stddiv = res.data.keyValues.Sd.toPrecision(4) + ' g CHO';
+                  this.min = res.data.keyValues.Min.toPrecision(4) + ' g CHO';
+                  this.max = res.data.keyValues.Max.toPrecision(4) + ' g CHO';
+                  break;
+                case 'BASAL':
+                  this.gcv = null
+                  this.gmi = null
+                  this.avg = res.data.keyValues.Average.toPrecision(4) + ' mU/min';
+                  this.stddiv = res.data.keyValues.Sd.toPrecision(4) + ' mU/min';
+                  this.min = res.data.keyValues.Min.toPrecision(4) + ' mU/min';
+                  this.max = res.data.keyValues.Max.toPrecision(4) + ' mU/min';
+                  break;
+                case 'BOLUS':
+                  this.gcv = null
+                  this.gmi = null
+                  this.avg = res.data.keyValues.Average.toPrecision(4) + ' U';
+                  this.stddiv = res.data.keyValues.Sd.toPrecision(4) + ' U';
+                  this.min = res.data.keyValues.Min.toPrecision(4) + ' U';
+                  this.max = res.data.keyValues.Max.toPrecision(4) + ' U';
+                  break;
+                default:
+                  this.gcv = null
+                  this.gmi = null                
+                  this.avg = res.data.keyValues.Average.toPrecision(4)
+                  this.stddiv = res.data.keyValues.Sd.toPrecision(4)
+                  this.min = res.data.keyValues.Min.toPrecision(4)
+                  this.max = res.data.keyValues.Max.toPrecision(4)
+              }
+          }
         })
     }
   },
